c4363668f8a757ea629e3017f08beea2e4c4f86c
ghazal-ansari <ghazalteen28@gmail.com>
2023-08-04 10:17:56 +0330

    2nd commit

---
+++ 03/03.py
@@ -0,0 +1,44 @@
+# a = int(input())
+# b = 0
+# while a != 0:
+#     d = a % 10
+#     a = a//10
+#     b = b*10+d
+# a = b
+# b = 0
+# while a != 0:
+#     d = a % 10
+#     a = a//10
+#     b = b*10+d
+#     print (f"{d}: ", end="")
+#     i = 0
+#     while i < d:
+#         print(d, end="")
+#         i+=1
+#     print ()
+
+# a = int(input())
+# b = int(input())
+# c = True
+# d = 2
+# while d <= a < b:
+
+#     if a or b % d == 0:
+#         c = False
+#         d += 1
+#     else:
+#         d += 1
+# if c==True:
+#     print(True)
+# else:
+#     print(False)
+
+
+# n = int(input())
+# i = 2
+# p = True
+# while i < n:
+#     if n % i == 0:
+#         p = False
+#     i += 1
+# print(p)
